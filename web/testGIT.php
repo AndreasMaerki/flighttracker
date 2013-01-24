cc0685eeb77d8a481dbd14988a7d5735c4500473
phil112233 <phil.roten@gmail.com>
2013-01-24 10:45:53 +0100

    jhfkhfkjhfkjdshfkhsdf

---
+++ web/testGIT.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of testGIT
+ *
+ * @author Philippe
+ */
+class testGIT {
+    //put your code here
+}
+
+?>
