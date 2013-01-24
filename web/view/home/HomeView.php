d6630553c4adf0f4ed65579ed8e456ab162226c3
Andreas Maerki <andy1111@me.com>
2013-01-24 23:41:03 +0100

    commit

--- web/view/home/HomeView.php
+++ web/view/home/HomeView.php
@@ -19,29 +19,29 @@ class HomeView extends View {
         echo "<form action={$nextFlightsURI} method=\"POST\">";
 
         // Flugzeugnummer suche
-        echo "<li class=\"formularMiddl\">";
+        echo "<div class=\"formularMiddl\">";
         echo "<label for=\"aircraftField:\">Flight Nr.:</label>";
         echo "<input type=\"search\" id=\"aircraftNrField\" name=\"aircraftField\">\n";
-        echo "</li>";
+        echo "</div>";
 
         // Ankunft
-        echo "<li id=\"formularLeft\">";
+        echo "<div id=\"formularLeft\">";
         echo "<label for=\"airportToField\:\">Airport To:</label>";
         echo "<input type=\"search\" class=\"airportField\" name=\"airportToField\">\n";
         echo "<label for=\"arrivalDateField\">Arrival Date:</label>";
         echo "<input type=\"search\" class=\"dateField\" name=\"arrivalDateField\">\n";
-        echo "</li>";
+        echo "</div>";
 
         // Abflug
-        echo "<li id=\"formularRight\">";
+        echo "<div id=\"formularRight\">";
         echo "<label for=\"airportFromField:\">Airport From:</label>";
         echo "<input type=\"search\" class=\"airportField\" name=\"airportFromField\">\n";
         echo "<label for=\"departDateField:\">Depart Date:</label>";
         echo "<input type=\"search\" class=\"dateField\" name=\"departDateField\">\n";
-        echo "</li>";
+        echo "</div>";
 
         // Filter
-        echo "<li class=\"formularDown\">";      
+        echo "<div class=\"formularDown\">";      
         echo "<label for=\"filter\">Filter:</label>";
         echo "<select type=\"search\" class=\"airportField\" name=\"filter\" size=\"1\">";
         for ($i=1;$i<16;$i++){
@@ -56,7 +56,7 @@ class HomeView extends View {
 
         // Button Find
         echo "<input class =\"button\"  type=\"submit\" name=\"search\" value=\"find\">";
-        echo "</form>\n</li>";
+        echo "</form>\n</div>";
         echo "</div>";
 
 
